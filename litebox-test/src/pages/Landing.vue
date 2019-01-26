8e898aab616ce24e15d03c17dac9edb84f94f39e
Juan <lazo.juan@hotmail.com>
2019-01-26 11:39:50 -0300

    inicio

---
+++ litebox-test/src/pages/Landing.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>soy la landing</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Homepage',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .homepage-container {
+
+  }
+
+</style>
